9e82c59f23ed82ebf437ed8ef2ee43d5c082d3c6
Rhys Sharrem <rhys.sharrem@gmail.com>
2016-07-13 20:12:39 +0100

    GettingStarted e2

--- exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedTests.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedTests.scala
@@ -3,13 +3,32 @@ package fpinscala.gettingstarted
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 import MyModule.fib
+import PolymorphicFunctions.isSorted
 
 class GettingStartedTests extends FlatSpec {
+
+  val compare = (a : Int, b : Int) => a <= b
 
   "fib" should "return the fibonacci values in order" in {
     fib(0) shouldBe 0
     fib(1) shouldBe 1
     fib(2) shouldBe 1
     fib(3) shouldBe 2
+  }
+
+  "isSorted" should "return true when an array is sorted according to the comparator" in {
+    isSorted[Int](Array(1, 2, 3), compare)
+  }
+
+  "isSorted" should "return false when an array is not according to the comparator" in {
+    isSorted[Int](Array(1, 2, 3, 1), compare)
+  }
+
+  "isSorted" should "return true for an empty Array" in {
+    isSorted[Int](Array(), compare)
+  }
+
+  "isSorted" should "return true for an Array with one element" in {
+    isSorted[Int](Array(1), compare)
   }
 }
